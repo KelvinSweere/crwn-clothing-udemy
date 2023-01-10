5d1627952a7ea4cae524a30492bc17817cfa13ef
Kelvin Sweere <kelvin.sweere@teamrockstars.nl>
2023-01-10 10:03:47 +0100

    Added base checkout component with redirect.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,20 @@
 import './cart-dropdown.component.scss'
 import { useContext } from 'react';
+import { useNavigate } from "react-router-dom";
 
 import { CartContext } from '../../context/cart.context'
 import CartItem from '../cart-item/cart-item.component';
 import Button from '../button/button.component';
+import Checkout from '../../routes/checkout/checkout.component';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
-    
+    const navigate = useNavigate();
+
+    const toCheckout = () => {
+        navigate('/checkout')
+    }
+
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
@@ -15,7 +22,7 @@ const CartDropdown = () => {
                     <CartItem key={item.id} cartItem={item}></CartItem>
                 )}
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick={toCheckout}>GO TO CHECKOUT</Button>
         </div>
     );
 }
